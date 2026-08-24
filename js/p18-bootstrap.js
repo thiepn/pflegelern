c76776123802cbf5d2ff708117c50651e7ec3140
thiepn <junsosmart@gmail.com>
2026-08-25 01:07:07 +0200

    P19: install legacy mistake migration before app init

--- js/p18-bootstrap.js
+++ js/p18-bootstrap.js
@@ -1,11 +1,13 @@
 import { initExamPlanUi, installExamPlanPatches } from './p16-exam-plan.js';
 import { installAdaptiveMixPatches } from './p17-study-mix.js';
 import { initMasteryInteractionBridge, installMasteryModelPatches } from './p18-mastery.js';
+import { installRemediationMigrationPatch } from './p19-remediation-migration.js';
 import { installWeaknessRemediationPatches } from './p19-remediation.js';
 
 await installExamPlanPatches();
 installAdaptiveMixPatches();
 installMasteryModelPatches();
+installRemediationMigrationPatch();
 installWeaknessRemediationPatches();
 initMasteryInteractionBridge();
 await import('./app.js');
